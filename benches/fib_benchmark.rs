7cc3a9a585f8c466569057ed8819e6971a9e0317
mattearl <578583+mattearl@users.noreply.github.com>
2024-10-02 01:18:03 -0400

    test: add bench tests to test the computation time for each of the fibonacci sequence computation methods

--- benches/fib_benchmark.rs
+++ benches/fib_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rust_fib::{fib, fib2, fib3, fib4};
+use fibonacci_assesment::fibonacci;
 use tokio::runtime::Builder;
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -8,19 +8,102 @@ fn criterion_benchmark(c: &mut Criterion) {
         .build()
         .expect("Creating runtime failed");
 
-    let size = black_box(40);
+    let size = black_box(100000);
 
-    c.bench_function(format!("fib {size}").as_str(), |b| {
-        b.to_async(&rt).iter(|| fib(size))
+    c.bench_function(format!("fib_seq_basic {size}").as_str(), |b| {
+        b.iter(|| fibonacci::seq_basic(size))
     });
 
-    c.bench_function(format!("fib2 {size}").as_str(), |b| {
-        b.to_async(&rt).iter(|| fib2(size))
+    c.bench_function(format!("fib_seq_hybrid_10000chunks {size}").as_str(), |b| {
+        b.iter(|| fibonacci::seq_hybrid(size, 10000))
     });
 
-    c.bench_function(format!("fib3 {size}").as_str(), |b| b.iter(|| fib3(size)));
+    c.bench_function(
+        format!("fib_seq_hybrid_kanal_1000chunks {size}").as_str(),
+        |b| b.iter(|| fibonacci::seq_hybrid_kanal(size, 1000)),
+    );
 
-    c.bench_function(format!("fib4 {size}").as_str(), |b| b.iter(|| fib4(size)));
+    c.bench_function(
+        format!("fib_seq_hybrid_rayon_500chunks {size}").as_str(),
+        |b| b.iter(|| fibonacci::seq_hybrid_rayon(size, 500)),
+    );
+    c.bench_function(
+        format!("fib_seq_hybrid_rayon_1000chunks {size}").as_str(),
+        |b| b.iter(|| fibonacci::seq_hybrid_rayon(size, 1000)),
+    );
+    c.bench_function(
+        format!("fib_seq_hybrid_rayon_1500chunks {size}").as_str(),
+        |b| b.iter(|| fibonacci::seq_hybrid_rayon(size, 1500)),
+    );
+
+    c.bench_function(format!("fib_hybrid_tokio_500chunks {size}").as_str(), |b| {
+        b.to_async(&rt)
+            .iter(|| fibonacci::seq_hybrid_tokio(size, 500, 20))
+    });
+    c.bench_function(
+        format!("fib_hybrid_tokio_1000chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_tokio(size, 1000, 20))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_tokio_1000chunks_30concurrent {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_tokio(size, 1000, 30))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_tokio_1500chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_tokio(size, 1500, 20))
+        },
+    );
+
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_700chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 700, 20))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_1000chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 1000, 20))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_1500chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 1500, 20))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_2000chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 2000, 20))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_1000chunks_30concurrent {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 1000, 30))
+        },
+    );
+    c.bench_function(
+        format!("fib_hybrid_kanal_tokio_5000chunks {size}").as_str(),
+        |b| {
+            b.to_async(&rt)
+                .iter(|| fibonacci::seq_hybrid_kanal_tokio(size, 5000, 20))
+        },
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
